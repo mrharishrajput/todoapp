93acccf8b48053a45790d2e39f4ecc3807785f5b
Harish Kumar <81924036+mrharishrajput@users.noreply.github.com>
2021-11-30 17:10:11 +0530

    Add files via upload
    final commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let a="";
+let todo=[];
+
+function task(){
+    let b = document.getElementById("input").value;
+    a+=b;
+}
+function addtodo(){
+    todo.push(a);
+    a="";
+    maping();
+    document.getElementById("input").value="";
+}
+
+
+function maping(){
+    document.getElementById("count").innerHTML=`My Total todo's are ${todo.length+1}`;
+    document.getElementById("mytodo").innerHTML = todo.map((i)=>{
+
+        return `<div>${i}</div>`
+    })
+}
